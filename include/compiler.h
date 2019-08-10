a6a538e43905d4fb943347a6540dc4305f49fc8f
sezero <sezero@users.sourceforge.net>
2019-08-10 15:24:50 +0300

    disable support for printf attribute on function pointers for old gcc.
    see: https://bugzilla.nasm.us/show_bug.cgi?id=3392600

--- include/compiler.h
+++ include/compiler.h
@@ -321,6 +321,12 @@ size_t strnlen(const char *s, size_t maxlen);
 #else
 # define printf_func(fmt, list)
 #endif
+/* http://bugs.debian.org/55298 - http://gcc.gnu.org/PR3481 */
+#if defined(__GNUC__) && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
+# define printf_funp printf_func
+#else
+# define printf_funp(fmt, list)
+#endif
 
 /*
  * How to tell the compiler that a function is pure arithmetic
